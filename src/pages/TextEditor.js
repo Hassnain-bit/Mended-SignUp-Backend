0ec65a4787e77a27083c9acbb604578ce3aaad7d
Hassnain-bit <m.hassnainkhan32@gmail.com>
2023-08-20 05:54:31 +0500

    highlighter

--- src/pages/TextEditor.js
+++ src/pages/TextEditor.js
@@ -6,8 +6,8 @@ function TextEditor() {
   const [underlineActive, setUnderlineActive] = useState(false);
   const [bulletActive, setBulletActive] = useState(false);
 
-  const handleCommand = (command) => {
-    document.execCommand(command, false, null);
+  const handleCommand = (command, value = null) => {
+    document.execCommand(command, false, value);
   };
 
   const handleToggleBold = () => {
@@ -30,6 +30,16 @@ function TextEditor() {
     handleCommand('insertUnorderedList');
   };
 
+  const handleToggleHighlight = () => {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    
+    const span = document.createElement('span');
+    span.style.backgroundColor = 'yellow';
+    
+    range.surroundContents(span);
+  };
+
   return (
     <div className="p-4">
       <div className='flex gap-6 mb-4'>
@@ -45,12 +55,11 @@ function TextEditor() {
         <button onClick={handleToggleBullet} className={bulletActive ? 'active' : ''}>
           •
         </button>
+        <button onClick={handleToggleHighlight} className={'highlight'}>
+          H
+        </button>
       </div>
-      {/* <div
-        contentEditable="true"
-        className="editor p-4 border"
-      /> */}
-       <div
+      <div
         contentEditable="true"
         className={`editor editor p-4 border ${bulletActive ? 'bullet-list' : ''}`}
       />
