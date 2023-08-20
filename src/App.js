1e67d72a86e73e0d0f6a45fad24e40f103638b7c
Hassnain-bit <m.hassnainkhan32@gmail.com>
2023-08-20 19:55:35 +0500

    delete text editor

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Protected from "./components/Protected";
 import { useState } from "react";
 import ImagePage from "./pages/ImagePage";
 import SubmitForm from "./pages/SubmitForm";
-import TextEditor from "./pages/TextEditor";
-
 function App() {
   const [isLoggedIn, setisLoggedIn] = useState(
     localStorage.getItem("isLoggedIn") === "true"
@@ -33,8 +31,6 @@ function App() {
             <Route path="/image" element={<ImagePage />} />
 
             <Route path="/submitForm" element={<SubmitForm />} />
-
-            <Route path="/textEditor" element={<TextEditor/>} />
           </Routes>
         </div>
       </BrowserRouter>
